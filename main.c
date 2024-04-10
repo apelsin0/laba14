f5ba4ce5f4bb5437b2bb7eed49178fa2aa026d5b
apelsin0 <pochtaapelsin@gmail.com>
2024-04-11 01:58:52 +0300

    append push / pop functions

--- main.c
+++ main.c
@@ -4,18 +4,3 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
-
-void test() {
-    test_pushBack_emptyVector();
-    test_pushBack_fullVector();
-// последующие тесты
-}
-
-void test_popBack_notEmptyVector() {
-    vector v = createVector(0);
-    pushBack(&v, 10);
-    assert(v.size == 1);
-    popBack(&v);
-    assert(v.size == 0);
-    assert(v.capacity == 1);
-}
